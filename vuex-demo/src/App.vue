ad34011761f77c9fd6d30edad99315bf6f4c3fb9
burning_33 <977690289@qq.com>
2020-05-25 18:07:13 +0800

    vuex-demo

---
+++ vuex-demo/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <Toolbar></Toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+    Toolbar: () => import('./components/Toolbar')
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ vuex-demo/src/components/Toolbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="toolbar">
+    <i class="glyphicon logo"><img src="../assets/logo.png" width="30" height="30" alt=""></i>
+    <i class="glyphicon glyphicon-plus"></i>
+    <i class="glyphicon glyphicon-star"></i>
+    <i class="glyphicon glyphicon-remove"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Toolbar',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
